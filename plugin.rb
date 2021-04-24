3287d293e4cab375252f1e816b7e7fa107c2a14e
Prasun Jaiswal <prasun.jaiswal@waycool.in>
2021-04-24 21:08:26 +0530

    added config files

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: guest-gate
+# name: guest-gate-prasunjais
 # about: Force guest users to create an account by preventing them from seeing more topics
 # version: 0.2.1
 # authors: Prasun Jaiswal (prasunjais@gmail.com)
