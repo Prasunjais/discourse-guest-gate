76ab5a715f85c7567a284cb99eee55dd3b96be0d
Prasun Jaiswal <prasun.jaiswal@waycool.in>
2021-04-24 20:07:29 +0530

    added plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: guest-gate
+# about: Force guest users to create an account by preventing them from seeing more topics
+# version: 0.2.1
+# authors: Prasun Jaiswal (prasunjais@gmail.com)
+# url: https://github.com/Prasunjais/discourse-guest-gate
